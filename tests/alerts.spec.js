0422a4438435eac7664423af98b2b96e6370e3fd
Mahesh <maheshwaranand06@gmail.com>
2026-06-23 23:33:18 +0530

    Uncommented Lines in alerts.spec.js

--- tests/alerts.spec.js
+++ tests/alerts.spec.js
@@ -1,34 +1,34 @@
 import {test} from '@playwright/test';
 
-// test('Simple alert',  async({page})=>{
-//   await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
-//   page.on('dialog', (dialogForAlert)=>{
-//     dialogForAlert.accept()
-//     console.log(dialogForAlert.message())
-//     console.log(dialogForAlert.type())
-//   })
-//   await page.locator('//button[text()="Click for JS Alert"]').click();
-//   await page.waitForTimeout(5000);
-// });
+test('Simple alert',  async({page})=>{
+  await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+  page.on('dialog', (dialogForAlert)=>{
+    dialogForAlert.accept()
+    console.log(dialogForAlert.message())
+    console.log(dialogForAlert.type())
+  })
+  await page.locator('//button[text()="Click for JS Alert"]').click();
+  await page.waitForTimeout(5000);
+});
 
-// test('Cancel and Confirm Alert', async ({ page }) => {
-//   await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+test('Cancel and Confirm Alert', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
 
-//   page.on('dialog', async (dialog) => {
-//     console.log(dialog.message());
-//     console.log(dialog.type());
+  page.on('dialog', async (dialog) => {
+    console.log(dialog.message());
+    console.log(dialog.type());
 
-//     if (dialog.type() === "confirm") {
-//       await dialog.accept();
-//     } 
-//     else {
-//       await dialog.dismiss();
-//     }
-//   });
+    if (dialog.type() === "confirm") {
+      await dialog.accept();
+    } 
+    else {
+      await dialog.dismiss();
+    }
+  });
 
-//   await page.getByText('Click for JS Confirm').click();
-//   await page.waitForTimeout(2000);
-// });
+  await page.getByText('Click for JS Confirm').click();
+  await page.waitForTimeout(2000);
+});
 
 
 // test('Prompt alert', async ({ page }) => {
